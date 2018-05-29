b0b8573ba8ca4bf4dd16ff975ca9d2b56c5365ca
wahab Qureshi <its.inevitable@hotmail.com>
2018-05-29 13:15:59 +0100

    JS done.

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
 <ol>
     <li class="strikethrough"><a href="index-simple.php">Hello World - Simple Assertions</a></li>
     <li class="strikethrough"><a href="index-form.php">Hello World - Forms</a></li>
-    <li class="strikethrough0"><a href="index-form-js.php">Hello JS World!</a></li>
+    <li class="strikethrough"><a href="index-form-js.php">Hello JS World!</a></li>
     <li class="strikethrough0"><a href="index-sql.php">Hello SQL World!</a></li>
 </ol>
 </body>
